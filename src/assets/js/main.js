bdf6ed94aec8d74cbc1909f65bc68d021f75e71b
MUNNA AHMED <msdmunna77@gmail.com>
2023-07-28 16:38:13 +0600

    feature page banner

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -2,40 +2,40 @@
 
 // Preloader
 $(window).on("load", function () {
-	$(".preloader").fadeOut(1000);
+  $(".preloader").fadeOut(1000);
 });
 
 // Menu Click Event
 let trigger = $(".header-trigger");
 let dropdown = $(".menu");
 if (trigger || dropdown) {
-	trigger.each(function () {
-		$(this).on("click", function (e) {
-			e.stopPropagation();
-			dropdown.toggleClass("active");
-			trigger.toggleClass("active");
-			$(".overlay").toggleClass("active-color");
-		});
-	});
-	dropdown.each(function () {
-		$(this).on("click", function (e) {
-			e.stopPropagation();
-		});
-	});
-	$(document).on("click", function () {
-		if (parseInt(screenSize) < parseInt(991)) {
-			dropdown.removeClass("active");
-			trigger.removeClass("active");
-			$(".overlay").removeClass("active-color");
-		}
-	});
+  trigger.each(function () {
+    $(this).on("click", function (e) {
+      e.stopPropagation();
+      dropdown.toggleClass("active");
+      trigger.toggleClass("active");
+      $(".overlay").toggleClass("active-color");
+    });
+  });
+  dropdown.each(function () {
+    $(this).on("click", function (e) {
+      e.stopPropagation();
+    });
+  });
+  $(document).on("click", function () {
+    if (parseInt(screenSize) < parseInt(991)) {
+      dropdown.removeClass("active");
+      trigger.removeClass("active");
+      $(".overlay").removeClass("active-color");
+    }
+  });
 }
 
 // Submenu Click Event
 $(".menu .menu-item .menu-link").on("click", function (e) {
-	if (parseInt(screenSize) < parseInt(991)) {
-		$(this).siblings(".sub-menu").slideToggle();
-	}
+  if (parseInt(screenSize) < parseInt(991)) {
+    $(this).siblings(".sub-menu").slideToggle();
+  }
 });
 
 //Menu Dropdown
@@ -44,45 +44,51 @@ $("ul>li>.sub-menu").parent("li").addClass("has-submenu");
 // Detect Screen Size
 let screenSize = window.innerWidth;
 window.addEventListener("resize", function (e) {
-	screenSize = window.innerWidth;
+  screenSize = window.innerWidth;
 });
 
 // Sticky Menu
 var header = document.querySelector(".header");
 if (header) {
-	window.addEventListener("scroll", function () {
-		header.classList.toggle("sticky", window.scrollY > 0);
-	});
+  window.addEventListener("scroll", function () {
+    header.classList.toggle("sticky", window.scrollY > 0);
+  });
 }
 
 // Scroll To Top Event
 var scrollTop = $(".scrollToTop");
 $(window).on("scroll", function () {
-	if ($(this).scrollTop() < 500) {
-		scrollTop.removeClass("active");
-	} else {
-		scrollTop.addClass("active");
-	}
+  if ($(this).scrollTop() < 500) {
+    scrollTop.removeClass("active");
+  } else {
+    scrollTop.addClass("active");
+  }
 });
 
 // Click event to scroll to top
 $(".scrollToTop").on("click", function () {
-	$("html, body").animate(
-		{
-			scrollTop: 0,
-		},
-		300
-	);
-	return false;
+  $("html, body").animate(
+    {
+      scrollTop: 0,
+    },
+    300
+  );
+  return false;
 });
 
 // Active Path Active
 var path = location.pathname.split("/");
 var current = location.pathname.split("/")[path.length - 1];
 $(".menu li a").each(function () {
-	if ($(this).attr("href").indexOf(current) !== -1 && current != "") {
-		$(this).addClass("active");
-	}
+  if ($(this).attr("href").indexOf(current) !== -1 && current != "") {
+    $(this).addClass("active");
+  }
 });
 
-new WOW().init();
+new WOW({
+  boxClass: "wow", // default
+  animateClass: "animated", // default
+  offset: 0, // default
+  mobile: false, // default
+  live: true, // default
+}).init();
